12a5a481fa17d0dd871f50756fd1206680dcd0d7
waly <65059215+wal8y@users.noreply.github.com>
2024-03-27 10:21:24 +0300

    Update s.js 2

--- public/s.js
+++ public/s.js
@@ -79,4 +79,16 @@ document.addEventListener("DOMContentLoaded", function () {
             reader.readAsDataURL(file);
         }
     });
+    async function convertToPNG(imageDataUrl) {
+        const img = new Image();
+        img.src = imageDataUrl;
+
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+        return canvas.toDataURL('image/png');
+    }
 });
